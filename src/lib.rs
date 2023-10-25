fe6ec9345b6fc4fcffc6d3b6b5f984fcc0b23c03
Scott Hyndman <scotty.hyndman@gmail.com>
2023-10-25 18:34:59 -0400

    Allow 7 or 10-bit addressing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 #![no_std]
 
-use embedded_hal_async::i2c::I2c;
+use embedded_hal_async::i2c::{AddressMode, I2c, SevenBitAddress};
 
-#[derive(Debug, Copy, Clone)]
-#[allow(non_camel_case_types)]
-pub enum Address {
-    ADDR_H = 0b101_1100, // 0x5c
-    ADDR_L = 0b010_0011, // 0x23
-}
+pub const DEFAULT_ADDRESS_ADAFRUIT: SevenBitAddress = 0x08;
 
 #[derive(Debug, Copy, Clone)]
 #[allow(non_camel_case_types)]
@@ -30,19 +25,32 @@ pub enum ContinuesMeasurement {
     HIHGT_RES2 = 0b0001_0001, // 0.5  lx resolution
     LOW_RES = 0b0001_0011,    // 4    lx resolution
 }
-pub struct BH1750<I2C, DELAY> {
+pub struct BH1750<I2C, A, DELAY> {
     com: I2C,
     delay: DELAY,
-    address: Address,
+    address: A,
 }
 
-impl<I2C, DELAY, E> BH1750<I2C, DELAY>
+impl<I2C, DELAY, E> BH1750<I2C, SevenBitAddress, DELAY>
 where
-    I2C: I2c<Error = E>,
+    I2C: I2c<SevenBitAddress, Error = E>,
     DELAY: embedded_hal_async::delay::DelayUs,
 {
     /// Create new BH1750 driver
-    pub fn new(i2c: I2C, delay: DELAY, address: Address) -> Result<Self, E> {
+    pub fn new(i2c: I2C, delay: DELAY) -> Result<Self, E> {
+        Self::new_with_address(i2c, delay, DEFAULT_ADDRESS_ADAFRUIT)
+    }
+}
+
+impl<I2C, A, DELAY, E> BH1750<I2C, A, DELAY>
+where
+    A: AddressMode + Copy,
+    I2C: I2c<A, Error = E>,
+    DELAY: embedded_hal_async::delay::DelayUs,
+{
+    /// Create new BH1750 driver
+    pub fn new_with_address(i2c: I2C, delay: DELAY, address: A) -> Result<Self, E> {
+        // AddressMode;
         let chip = BH1750 {
             com: i2c,
             delay,
@@ -93,14 +101,12 @@ where
 
     async fn send_instruction(&mut self, instr: u8) {
         let mut buffer = [0];
-        let _ = self
-            .com
-            .write_read(self.address as u8, &[instr], &mut buffer);
+        let _ = self.com.write_read(self.address, &[instr], &mut buffer);
     }
 
     async fn resive_answer(&mut self, instr: u8) -> u16 {
         let mut data: [u8; 2] = [0; 2];
-        let _ = self.com.write_read(self.address as u8, &[instr], &mut data);
+        let _ = self.com.write_read(self.address, &[instr], &mut data);
         let raw_answer: u16 = ((data[0] as u16) << 8) | data[1] as u16;
         raw_answer
     }
